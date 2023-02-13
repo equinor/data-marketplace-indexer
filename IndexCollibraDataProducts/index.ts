e9449c8d66e29d831d0d1b41fe63a2798735a248
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-02-13 09:46:38 +0100

    🔧 Add Prettier (ab#99938)

--- IndexCollibraDataProducts/index.ts
+++ IndexCollibraDataProducts/index.ts
@@ -3,7 +3,7 @@ import { AzureFunction, Context } from '@azure/functions'
 const timerTrigger: AzureFunction = async function (
   context: Context,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  myTimer: any
+  myTimer: any,
 ): Promise<void> {
   const timeStamp = new Date().toISOString()
 
