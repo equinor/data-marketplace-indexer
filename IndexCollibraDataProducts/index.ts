629dbb9cd4faf2fc7ccee79f5472c13e65b8d74b
Wenche Tollevsen <wenche.tollevsen@gmail.com>
2023-02-17 11:42:17 +0100

    ♻️ Use fp-ts to add mocked data

--- IndexCollibraDataProducts/index.ts
+++ IndexCollibraDataProducts/index.ts
@@ -1,18 +1,19 @@
 import { AzureFunction, Context } from '@azure/functions'
-import algoliasearch from 'algoliasearch'
+// import algoliasearch from 'algoliasearch'
 import DotenvAzure from 'dotenv-azure'
-//import * as E from 'fp-ts/lib/Either'
-//import { ap } from 'fp-ts/lib/Identity'
-//import * as T from 'fp-ts/lib/Task'
-//import * as TE from 'fp-ts/lib/TaskEither'
-//import { pipe, flow } from 'fp-ts/lib/function'
-//import { update, generateIndexName } from '../common/algolia'
-//import { getEnvironment } from '../common/env'
+import * as E from 'fp-ts/lib/Either'
+import { ap } from 'fp-ts/lib/Identity'
+import * as T from 'fp-ts/lib/Task'
+import * as TE from 'fp-ts/lib/TaskEither'
+import { pipe, flow } from 'fp-ts/lib/function'
+
+import { update, generateIndexName } from '../common/algolia'
+import { getEnvironment } from '../common/env'
 
 import { indexSettings } from './src/indexSettings'
-import { /* fetchData, */ mockData } from './src/mockProducts'
+import { fetchData /* , mockData */ } from './src/mockProducts'
 
-// const indexIdentifier = 'Collibra'
+const indexIdentifier = 'Collibra'
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
   await new DotenvAzure().config({
@@ -21,30 +22,18 @@ const httpTrigger: AzureFunction = async function (context: Context): Promise<vo
   })
   const logger = context.log
 
-  const indexName = 'test_Collibra'
-  // const indexName = flow(getEnvironment, E.map(generateIndexName(indexIdentifier)))
-  logger('Index name', indexName)
+  const indexName = flow(getEnvironment, E.map(generateIndexName(indexIdentifier)))
+  const updateAlgolia = flow(indexName, E.map(flow(update, ap(indexSettings))))
 
-  const appId = process.env.ALGOLIA_APP_ID || ''
-  const apiKey = process.env.ALGOLIA_API_KEY || ''
-  logger('Credentials', appId, apiKey)
-  console.log('Creating client')
-  const client = algoliasearch(appId, apiKey)
-  const index = client.initIndex(indexName)
-  console.log('inited index')
-  index.setSettings(indexSettings)
-  console.log('added index settings')
-  await index.saveObjects(mockData)
-  console.log('added data')
-  logger('Updating done!')
-  //const updateAlgolia = flow(indexName, E.map(flow(update, ap(indexSettings))))
-
-  /*  pipe(
+  const run = pipe(
     fetchData(),
     TE.chainW((data) => pipe(updateAlgolia(), E.ap(E.of(data)), TE.fromEither)),
     TE.flatten,
     T.map(E.fold(console.error, console.log)),
-  ) */
+  )
+
+  run().catch(logger.error)
+  logger('Updating done!')
 }
 
 export default httpTrigger
--- IndexCollibraDataProducts/src/mockProducts.ts
+++ IndexCollibraDataProducts/src/mockProducts.ts
@@ -1,19 +1,35 @@
-//import * as E from 'fp-ts/lib/Either'
-//import * as TE from 'fp-ts/lib/TaskEither'
-// import { pipe } from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/lib/TaskEither'
 
 // @TOOO Get from models
 import type { CollibraAsset } from '../../common/types'
 
 export const mockData: CollibraAsset[] = [
   {
-    objectID: 'be3a48195ff1f_dashboard_generated_id',
+    objectID: 'be3a48195f4431f_dashboard_generated_id',
     createdAt: '2022-08-12T07:41:18.181Z',
     excerpt: 'A test data set',
     description: 'This data contains some test data',
-    id: 'af6f7548-124c-4cf5-9ca5-aa216a60a53d',
-    name: 'Test data',
+    id: 'af6f7548-124c-4cf5-9ca5-aa2sdkf16a60a53d',
+    name: 'Test data with fp-ts',
     provider: 'Collibra',
+    qualityScore: 0,
+    tags: ['Demand', 'Capacity', 'Supply'],
+    community: ['Test department'],
+    type: 'Data Product',
+    updatedAt: '2022-12-03T07:06:11.347Z',
+    updateFrequency: 'Hourly',
+    dataOfficeAdmin: ['Ola Nordmann', 'Kari Andersen'],
+    dataSteward: ['Kari Andersen'],
+    owner: ['Ola Nordmann'],
+  },
+  {
+    objectID: 'be3a48195f5451f_dashboard_generated_id',
+    createdAt: '2022-08-12T07:41:18.181Z',
+    excerpt: 'A test data set',
+    description: 'This data contains some test data',
+    id: 'af6f7548-124c-4cf5-9ca5-aa216a60dsd53d',
+    name: 'Test data with fp-ts come on',
+    provider: 'PowerBI',
     qualityScore: 0,
     tags: ['Demand', 'Capacity', 'Supply'],
     community: ['Test department'],
@@ -26,10 +42,7 @@ export const mockData: CollibraAsset[] = [
   },
 ]
 
-//type FetchDataType = () => TE.TaskEither<Error, CollibraAsset[]>
+type FetchDataType = () => TE.TaskEither<Error, CollibraAsset[]>
 
-//const fetch: FetchDataType = TE.of(mockData)
-
-//export const fetchData = fetch()
-
-export const fetchData = () => mockData
+export const fetch: TE.TaskEither<Error, CollibraAsset[]> = TE.of(mockData)
+export const fetchData: FetchDataType = () => fetch
